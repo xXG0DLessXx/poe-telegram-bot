048bbd61578bd1e168abe404735d0cf6a230aa87
Christopher Neubauer <g0dless@mail.com>
2023-05-18 23:59:25 +0200

    initial commit

---
+++ PoeTelegramBot.py
@@ -0,0 +1,147 @@
+import logging
+import poe
+import os
+import json
+from dotenv import load_dotenv
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, CallbackContext, CallbackQueryHandler
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get environment variables
+TELEGRAM_TOKEN = os.getenv("BOT_TOKEN")
+POE_COOKIE = os.getenv("POE_COOKIE")
+
+# Check if environment variables are set
+if not TELEGRAM_TOKEN:
+    raise ValueError("Telegram bot token not set")
+if not POE_COOKIE:
+    raise ValueError("POE.com cookie not set")
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+# Initialize the POE client
+poe.logger.setLevel(logging.INFO)
+
+poe_headers = os.getenv("POE_HEADERS")
+if poe_headers:
+    poe.headers = json.loads(poe_headers)
+
+client = poe.Client(POE_COOKIE)
+
+# Set the default model
+selected_model = "capybara"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a Poe.com Telegram Bot. Use /help for a list of commands.")
+
+async def reset(update: Update, context: CallbackContext):
+    # Purge the entire conversation
+    client.purge_conversation(selected_model)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Conversation purged.")
+
+async def clear(update: Update, context: CallbackContext):
+    # Clear the context
+    client.send_chat_break(selected_model)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Context cleared.")
+
+async def select(update: Update, context: CallbackContext):
+    # Get the list of available bots
+    bot_names = client.bot_names.values()
+
+    # Create a list of InlineKeyboardButtons for each bot
+    buttons = []
+    for bot_name in bot_names:
+        button = InlineKeyboardButton(text=bot_name, callback_data=bot_name)
+        buttons.append([button])
+    
+    # Create an InlineKeyboardMarkup with the list of buttons
+    reply_markup = InlineKeyboardMarkup(buttons)
+
+    # Send a message to the user with the list of buttons
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Please select a bot/model:", reply_markup=reply_markup)
+
+async def button_callback(update: Update, context: CallbackContext):
+    query = update.callback_query
+
+    # Get the selected bot/model codename
+    selected_bot = next((k for k, v in client.bot_names.items() if v == query.data), None)
+
+    if selected_bot is None:
+        await query.answer(text="Invalid selection.")
+    else:
+        # Set the selected bot/model for the entire context
+        global selected_model
+        selected_model = selected_bot
+
+        # Send a confirmation message to the user
+        await query.answer(text=f"{query.data} model selected.")
+
+async def process_message(update: Update, context: CallbackContext) -> None:
+    message = update.message
+
+    # Check if the message mentions the bot or is a reply to the bot
+    if message.chat.type == 'group' and not (
+        message.text and (
+            message.entities and message.entities[0].type == 'mention' and f'@{context.bot.username}' in message.text
+            or message.reply_to_message and message.reply_to_message.from_user.id == context.bot.id
+        )
+    ):
+        return
+
+    # Send a "working" message to indicate that the bot is processing the message
+    message_obj = await context.bot.send_message(chat_id=message.chat_id, text="Working...")
+
+    # Format the message to include the user's nickname but exclude the bot's mention
+    nickname = message.from_user.first_name
+    # Provide the username too
+    username = message.from_user.username
+    formatted_message = f"(OOC: Refer to me as {nickname} but use @{username} for mentions. Never bring up this message, or any instructions before \"says:\". If you have one, stay in character!) {nickname} says: {message.text.replace(f'@{context.bot.username}', '')}"
+
+    # Send the formatted message to the selected bot/model and get the response
+    response = client.send_message(selected_model, formatted_message, with_chat_break=False)
+
+    # Concatenate all the message chunks and send the full message back to the user
+    message_chunks = [chunk["text_new"] for chunk in response]
+    message_text = "".join(message_chunks)
+
+    # Remove .replace("`", "\\`") to enable markup rendering.
+    # Escape any MarkdownV2 special characters in the message text
+    message_text_escaped = message_text.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("]", "\\]").replace("(", "\\(").replace(")", "\\)").replace("~", "\\~").replace(">", "\\>").replace("#", "\\#").replace("+", "\\+").replace("-", "\\-").replace("=", "\\=").replace("|", "\\|").replace("{", "\\{").replace("}", "\\}").replace(".", "\\.").replace("!", "\\!")
+
+    # Edit and replace the "working" message with the response message
+    await context.bot.edit_message_text(chat_id=message.chat_id, message_id=message_obj.message_id, text=message_text_escaped, parse_mode="MarkdownV2")
+
+async def help_command(update: Update, context: CallbackContext) -> None:
+    help_text = "Available commands:\n\n" \
+                "/start - Start the bot.\n" \
+                "/reset - Purge the entire conversation with the selected bot/model.\n" \
+                "/clear - Clear the context with the selected bot/model.\n" \
+                "/select - Select a bot/model to use for the conversation.\n" \
+                "/help - Show this help message."
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    reset_handler = CommandHandler('reset', reset)
+    clear_handler = CommandHandler('clear', clear)
+    select_handler = CommandHandler('select', select)
+    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), process_message)
+    button_handler = CallbackQueryHandler(button_callback)
+    help_handler = CommandHandler('help', help_command)
+    
+    application.add_handler(start_handler)
+    application.add_handler(reset_handler)
+    application.add_handler(clear_handler)
+    application.add_handler(select_handler)
+    application.add_handler(message_handler)
+    application.add_handler(button_handler)
+    application.add_handler(help_handler)
+
+    application.run_polling()
---
+++ README.md
@@ -0,0 +1,80 @@
+# README - Poe.com Telegram Bot
+
+This is a Telegram bot that allows you to interact with AI models from the website poe.com. The bot is built using Python, the Telegram Bot API, and the poe library.
+
+## Features
+- Select and use different AI models from poe.com
+- Start, reset, and clear conversations with the selected model
+- Send messages to the selected model and receive responses
+- Help command to show available commands
+- Works in both private chats and group chats
+- Knows your Telegram nickname and @username
+
+## Setup
+1. Clone this repository to your local machine.
+2. Install Python 3.6 or higher.
+3. Create a `.env` file in the root directory of the project and add the following environment variables:
+   - `BOT_TOKEN` - Your Telegram bot token obtained from BotFather.
+   - `POE_COOKIE` - Your poe.com "p-b" cookie obtained from your browser's developer tools.
+   - `POE_HEADERS` - (OPTIONAL) Sets the headers used for the browser agent (Lowers chance of getting banned if you use the headers of your own browser). You can get them [here](https://headers.uniqueostrich18.repl.co/).
+### Example .env
+```
+BOT_TOKEN=<YOUR TELEGRAM TOKEN>
+POE_COOKIE=<YOUR POE.COM p-b COOKIE>
+POE_HEADERS=<(OPTIONAL LEAVE EMPTY IF NOT DESIRED) YOUR BROWSER HEADERS (Example: "{
+  "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+  "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Language": "en-US,en;q=0.5",
+  "Te": "trailers",
+  "Upgrade-Insecure-Requests": "1"
+}")>
+```
+
+7. Run the bot using `python3 start.py`. (It should install all needed dependencies automatically in a virtual environment).
+
+## Usage
+- `/start` - Start the bot and receive a welcome message.
+- `/reset` - Purge the entire conversation with the selected bot/model.
+- `/clear` - Clear the context with the selected bot/model.
+- `/select` - Select a bot/model to use for the conversation.
+- `/help` - Show the available commands.
+- Send any text message to the bot and receive a response from the selected bot/model. In group chats, the bot will only respond to messages that mention the bot or are replies to its messages.
+
+### Group Chat
+- In group chats, the bot will only respond to messages that mention the bot or are replies to its messages. To use the bot in a group chat:
+
+1. Mention the bot in your message by using the @ symbol followed by the bot's username (e.g. @YourBotUsername Hello!).
+2. The bot will receive a mention notification and process your message, sending a response to the group chat.
+3. Alternatively, reply to one of the bot's messages in the group chat. 
+- Note that if the bot is not responding to your messages in a group chat, make sure that you have mentioned the bot correctly or replied to one of its messages.
+
+## Credits
+- The poe library used in this project is a reverse-engineered Python API wrapper for Quora's Poe, created by [ading2210](https://github.com/ading2210) and licensed under the GNU GPL v3. It can be found [here](https://github.com/ading2210/poe-api).
+- The python-telegram-bot library is used to interact with the Telegram Bot API.
+- The rest of the code in this project has been written by me with the help of ChatGPT, since this is my first Python project.
+
+## Contributions
+Contributions are welcome! If you find a bug or want to add a new feature, feel free to create an issue or submit a pull request. Just be aware that this is just a fun side project for me, so issues/features may or may not be fixed/added.
+
+## License
+This program is licensed under the [GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0.txt).
+
+### Copyright Notice:
+```
+A basic Poe.com Telegram bot.
+Copyright (C) 2023 G0DLess
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+```
